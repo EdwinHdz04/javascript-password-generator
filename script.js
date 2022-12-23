23395d7726470064833a9d85c3c37d5c91e7c4d3
Edwin Hernandez <edwinhernandez80@icloud.com>
2022-12-22 19:03:16 -0800

    added prompts

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var characterLength = 8;
+var choiceArr =[];
 
-
+var specialCharArr = ['!','@','#','$','%','^','&','*','(',')','[',']','{','}','|','?','/','<','>'];
+var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Y'];
+var numberArr =['1','2','3','4','5','6','7','8','9','0',];
 
 // Write password to the #password input
 function writePassword() {
@@ -15,12 +20,18 @@ function writePassword() {
 // I click the button to generate a password
 function generatePassword(){
   console.log("button works!")
- 
+
    return "Generated password"
  }
 
+ function getPrompts(){
+   characterLength = parseInt(prompt("How many characters would you want your password be?" ('8 - 128 characters'))
 
-
+    if(isNaN(characterLength)|| characterLength <8 || characterLength > 128 ){
+       alert('Character lenght has to be a number, 8 -128 digits. TRY AGAIN.');
+       return false;
+    }
+ }
 // I am presented with a series of prompts for password criteria
 // prompted for password criteria
 
