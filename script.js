87fd68bfb18597398ee57aea3427189e98034dac
Edwin Hernandez <edwinhernandez80@icloud.com>
2022-12-22 21:38:57 -0800

    finished product

--- script.js
+++ script.js
@@ -8,50 +8,65 @@ var lowerCassArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 var uppercaseArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','Y'];
 var numberArr =['1','2','3','4','5','6','7','8','9','0',];
 
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
+ // I need a new, secure password
+
 function writePassword() {
-  // I need a new, secure password
-  var password = generatePassword();
+  var correctPrompts = GetPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  if (correctPrompts) { 
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+    
+
+  }
 }
 
 // I click the button to generate a password
 function generatePassword(){
-  console.log("button works!")
 
-   return "Generated password"
- }
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = (Math.floor.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  }
+  return password;
+  }
+   
 
 // I am presented with a series of prompts for password criteria
 // prompted for password criteria
 function GetPrompts() {
-  characterLength = parseInt(prompt ("How man charactares do you want your password to be? (8 - 128 characters"));
-
+  choiceArr = [];
+  characterLength = parseInt(prompt ("How man characters do you want your password to be? (8 - 128 characters"));
+// I choose a length of at least 8 characters and no more than 128 characters
   if (isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
     alert("Character Length has to be a number, 8 - 128 digits. TRY AGAIN.");
     return false;
   }
-  if (comfirm("Would you like lowercase letters in your password?")) {
+  if (confirm("Would you like lowercase letters in your password?")) {
     choiceArr = choiceArr.concat(lowerCassArr);
   }
-  if (comfirm("Would you like uppercase letters in your password?")) {
+  if (confirm("Would you like uppercase letters in your password?")) {
     choiceArr = choiceArr.concat(uppercaseArr);
   }
-  if (comfirm("Would you like special characters in your password?")) {
+  if (confirm("Would you like special characters in your password?")) {
     choiceArr = choiceArr.concat(specialCharArr);
   }
-  if (confirm("Would you like mnumbers in your password?")) {
+  if (confirm("Would you like numbers in your password?")) {
     choiceArr = choiceArr.concat(numberArr);
   }
   return true;
-  
+
 }
 
  
-
-
 // I select which criteria to include in the password
 // I choose a length of at least 8 characters and no more than 128 characters
 // asked for character types to include in the password
@@ -62,6 +77,6 @@ function GetPrompts() {
 // a password is generated that matches the selected criteria
 // the password is generated
 // the password is either displayed in an alert or written to the page
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
 
